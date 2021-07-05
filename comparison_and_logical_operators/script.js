9159ae62ebaf90b0c26895b3b61a34e33ca871cd
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 22:03:22 +0200

    50procent

---
+++ comparison_and_logical_operators/script.js
@@ -0,0 +1,26 @@
+const age = 19;
+if (age >= 18) {
+  console.log("U mag naar binnen!");
+} else {
+  console.log("Blijf thuis!");
+}
+
+const isFemale = false;
+if (isFemale == true) {
+  console.log("Veel plezier op de ladiesnight!");
+} else {
+  console.log("Zoek een ander feest!");
+}
+
+const driveStatus = "bob";
+if (driveStatus == "bob") {
+  console.log("U mag rijden!");
+} else {
+  console.log("Bestel een taxi!");
+}
+
+if (age > 18 && age < 25) {
+  console.log("Je krijgt 50% korting!");
+} else {
+  console.log("Je hoeft maar 100% te betalen!");
+}
