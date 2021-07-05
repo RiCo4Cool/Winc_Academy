ab6e4d9d1f59202946db63e0009a9d0b9bfe5e73
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 15:46:04 +0200

    ladies

--- gebruik_if_else_statements/script.js
+++ gebruik_if_else_statements/script.js
@@ -1,4 +1,15 @@
-const age = 23;
+const age = 17;
 if (age >= 18) {
   console.log("U mag naar binnen!");
+} else {
+  console.log("Blijf thuis!");
 }
+
+const isFemale = false;
+if (isFemale == true) {
+  console.log("Veel plezier op de ladiesnight!");
+} else {
+  console.log("Zoek een ander feest!");
+}
+
+const driveStatus = "bob";
