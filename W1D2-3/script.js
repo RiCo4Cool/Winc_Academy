0907d3305c0c0aba12f66aea2cabdec3a9599c7a
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 14:38:38 +0200

    divide

---
+++ W1D2-3/script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy Studenten");
+let numberOne = 1230941;
+let numberTwo = 1823794;
+let uitkomstOne = numberOne * numberTwo;
+console.log(uitkomstOne);
+let numberThree = 637263;
+let numberFour = 54;
+let uitkomstTwo = numberThree / numberFour;
+console.log(uitkomstTwo);
