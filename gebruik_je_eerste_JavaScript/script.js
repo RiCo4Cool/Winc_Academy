44eeb43b70ed50ff6cfd060ed8f6734b90e1c98b
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 13:46:05 +0200

    scriptfile

---
+++ gebruik_je_eerste_JavaScript/script.js
@@ -0,0 +1,23 @@
+console.log("Hello Winc Academy");
+let myName = "Richard";
+console.log(myName);
+/* Variabele maken met het reserved keyword 'if'
+        geeft een foutmelding */
+let numberOne = 1;
+let numberTwo = 2;
+let uitkomstNumbers = numberOne + numberTwo;
+console.log(uitkomstNumbers);
+let numberThree = "1";
+let numberFour = "2";
+let uitkomstStrings = numberThree + numberFour;
+console.log(uitkomstStrings);
+let numberFive = 3;
+let numberSix = 4;
+let uitkomstOperatorsOne = numberFive + numberSix * numberFive;
+console.log(uitkomstOperatorsOne);
+let uitkomstOperatorsTwo = (numberFive / numberSix) % numberFive;
+console.log(uitkomstOperatorsTwo);
+let ageNum = 44;
+console.log(typeof ageNum);
+let ageText = "vierenveertig";
+console.log(typeof ageText);
