a3bbb074d59fbc6623445590ec6aebd1c1024405
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 23:43:01 +0200

    kattenrassen

--- reference_types_objects_and_arrays/script.js
+++ reference_types_objects_and_arrays/script.js
@@ -1,3 +1,6 @@
+const breeds = require("./catbreeds.js");
+const catBreeds = breeds.catBreeds;
+
 let person = {
   name: "Richard",
   age: 44,
@@ -22,3 +25,12 @@ chosenColors.push("geel");
 chosenColors.push(5);
 chosenColors.push('{greeting: "Hi, ik ben een object"}');
 console.log(chosenColors[chosenColors.length - 1]);
+
+console.log("Naam kattensoort 3:", catBreeds[catBreeds.length - 1]["name"]);
+console.log("Energylevel kattensoort 1", catBreeds[0]["energy_level"]);
+console.log("Eerste temperament kat 2:", catBreeds[1]["temperament"][0]);
+console.log(
+  "Laatste temperament kat 3:",
+  catBreeds[2]["temperament"][catBreeds[2]["temperament"].length - 1]
+);
+console.log("Favo voedsel van kat 3:", catBreeds[2]["food"]["favourite_food"]);
