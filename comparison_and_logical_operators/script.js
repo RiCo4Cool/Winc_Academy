dbd3531c428436c7d2477da935a42360bf4bd3f3
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 22:07:31 +0200

    ludiekeactie

--- comparison_and_logical_operators/script.js
+++ comparison_and_logical_operators/script.js
@@ -1,4 +1,6 @@
 const age = 19;
+const firstName = "Bram";
+
 if (age >= 18) {
   console.log("U mag naar binnen!");
 } else {
@@ -23,4 +25,10 @@ if (age > 18 && age < 25) {
   console.log("Je krijgt 50% korting!");
 } else {
   console.log("Je hoeft maar 100% te betalen!");
+}
+
+if (firstName === "Sarah" || firstName === "Bram") {
+  console.log("Haal snel je gratis biertje!");
+} else {
+  console.log("Vergeet niet je biertje te betalen!");
 }
