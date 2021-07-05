75b6aaeda67ddc23670e23a59ee04b32cdcde8ef
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 23:08:14 +0200

    greeting

--- reference_types_objects_and_arrays/script.js
+++ reference_types_objects_and_arrays/script.js
@@ -2,3 +2,23 @@ let person = {
   name: "Richard",
   age: 44,
 };
+
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person["name"]);
+console.log(person["age"]);
+
+person.evaluations = [7, 10, 19];
+console.log(person.evaluations); // waarde is een array
+
+var chosenColors = [];
+chosenColors = ["groen", "blauw", "rood"];
+console.log(chosenColors);
+console.log(chosenColors.length);
+console.log(chosenColors[0]);
+console.log(chosenColors[chosenColors.length - 1]);
+chosenColors.push("geel");
+chosenColors.push(5);
+chosenColors.push('{greeting: "Hi, ik ben een object"}');
+console.log(chosenColors[chosenColors.length - 1]);
