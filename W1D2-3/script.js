9d577db78292c52f4064ee21115fb715edcc1767
Richard Cornelissen <ricorn@gmail.com>
2021-07-05 14:44:31 +0200

    comments

--- W1D2-3/script.js
+++ W1D2-3/script.js
@@ -1,4 +1,5 @@
 console.log("Hallo Winc Academy Studenten");
+// Dit is een grote som
 let numberOne = 1230941;
 let numberTwo = 1823794;
 let uitkomstOne = numberOne * numberTwo;
@@ -7,3 +8,19 @@ let numberThree = 637263;
 let numberFour = 54;
 let uitkomstTwo = numberThree / numberFour;
 console.log(uitkomstTwo);
+/* Uitkomst van de regel comment is niet zichtbaar
+in de console, want comments zijn niet zichtbaar
+in de console */
+/*
+let numberOne = 1230941;
+let numberTwo = 1823794;
+let uitkomstOne = numberOne * numberTwo;
+console.log(uitkomstOne);
+let numberThree = 637263;
+let numberFour = 54;
+let uitkomstTwo = numberThree / numberFour;
+console.log(uitkomstTwo);
+*/
+/* De uitkomst van de som blijft zichtbaar in
+de console, alleen de gedeeltes in de comments
+zijn niet meer zichtbaar */
