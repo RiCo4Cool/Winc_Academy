4f48c40e1b3445b6328c2142e68d5786f9bba719
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 12:24:22 +0200

    threetimes

---
+++ function_basics_exercise/script.js
@@ -0,0 +1,9 @@
+const makePizza = function () {
+  console.log("Make the pizza base");
+  console.log("Make the topping");
+  console.log("Use the oven");
+};
+
+makePizza();
+makePizza();
+makePizza();
