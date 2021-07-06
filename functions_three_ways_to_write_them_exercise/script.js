3fd80cf80b5c2b695b6e771b7f986ada1d482e13
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 16:05:28 +0200

    decl

--- functions_three_ways_to_write_them_exercise/script.js
+++ functions_three_ways_to_write_them_exercise/script.js
@@ -1,5 +1,9 @@
-function calculateNumbers(numberOne, numberTwo) {
+function declaration(numberOne, numberTwo) {
   const numberOneSq = numberOne * numberOne;
   const numberTwoSq = numberTwo * numberTwo;
   const addedSqNums = numberOneSq + numberTwoSq;
+  const sqAddedSqNums = addedSqNums * addedSqNums;
+  return sqAddedSqNums;
 }
+
+console.log(declaration(4, 8));
