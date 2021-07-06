50ed50f64d300382c27d504ce1a24559f4b03549
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 16:07:50 +0200

    arrow

--- functions_three_ways_to_write_them_exercise/script.js
+++ functions_three_ways_to_write_them_exercise/script.js
@@ -6,4 +6,22 @@ function declaration(numberOne, numberTwo) {
   return sqAddedSqNums;
 }
 
+const expression = function declaration(numberOne, numberTwo) {
+  const numberOneSq = numberOne * numberOne;
+  const numberTwoSq = numberTwo * numberTwo;
+  const addedSqNums = numberOneSq + numberTwoSq;
+  const sqAddedSqNums = addedSqNums * addedSqNums;
+  return sqAddedSqNums;
+};
+
+const arrow = (numberOne, numberTwo) => {
+  const numberOneSq = numberOne * numberOne;
+  const numberTwoSq = numberTwo * numberTwo;
+  const addedSqNums = numberOneSq + numberTwoSq;
+  const sqAddedSqNums = addedSqNums * addedSqNums;
+  return sqAddedSqNums;
+};
+
 console.log(declaration(4, 8));
+console.log(expression(4, 8));
+console.log(arrow(4, 8));
