0858b7a74c4dbcf5bf7b48dc0bf18d59f964c23e
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 14:13:25 +0200

    wallname

--- function_arguments_exercise/script.js
+++ function_arguments_exercise/script.js
@@ -1,7 +1,7 @@
-const paintWalls = function (color) {
-  console.log("The wall has been painted " + color);
+const paintWalls = function (color, wallName) {
+  console.log("The " + wallName + " has been painted " + color);
 };
 
-paintWalls();
+paintWalls("yellow", "north wall");
 /* If we call the function without an argument,
 the color will be printed as undefined */
