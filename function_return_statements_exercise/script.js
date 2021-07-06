3dd0bceb745b539c6c89e70f9dc154b6ee47229a
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 15:13:41 +0200

    bigNumber

---
+++ function_return_statements_exercise/script.js
@@ -0,0 +1,10 @@
+const bigNumber = function (number) {
+  if (number > 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(bigNumber(99));
+// This is a function that produces something
