dc013596e299dcbc11d999b321fcb43a373a47a2
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 16:03:03 +0200

    calculatenumb

---
+++ functions_three_ways_to_write_them_exercise/script.js
@@ -0,0 +1,5 @@
+function calculateNumbers(numberOne, numberTwo) {
+  const numberOneSq = numberOne * numberOne;
+  const numberTwoSq = numberTwo * numberTwo;
+  const addedSqNums = numberOneSq + numberTwoSq;
+}
