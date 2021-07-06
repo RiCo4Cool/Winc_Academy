4c52e74af28e6465a00cfcecd9725946bbfd2070
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 15:33:03 +0200

    average

--- function_return_statements_exercise/script.js
+++ function_return_statements_exercise/script.js
@@ -23,3 +23,19 @@ const bouncer = function (maxNum, curNum, age) {
 };
 
 console.log(bouncer(70, 50, 22));
+// this is a function that does something
+
+const calculateAverage = function (
+  numberOne,
+  numberTwo,
+  numberThree,
+  numberFour,
+  numberFive
+) {
+  return Math.round(
+    (numberOne + numberTwo + numberThree + numberFour + numberFive) / 5
+  );
+};
+
+console.log(calculateAverage(5, 2, 3, 4, 5));
+// this function produces something
