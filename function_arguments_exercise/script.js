d0df8cba865905080234a0f90325ed871c9eb70c
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 14:05:25 +0200

    green

---
+++ function_arguments_exercise/script.js
@@ -0,0 +1,7 @@
+const paintWalls = function (color) {
+  console.log("The wall has been painted " + color);
+};
+
+paintWalls();
+/* If we call the function without an argument,
+the color will be printed as undefined */
