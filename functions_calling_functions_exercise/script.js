e9f293935599f93ad928cc45e4444bc9de2eef07
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 22:54:02 +0200

    exercise2

--- functions_calling_functions_exercise/script.js
+++ functions_calling_functions_exercise/script.js
@@ -29,3 +29,12 @@ function priceIncludingVat(baseprice, vatpercentage) {
 console.log(priceIncludingVat(1000, 21));
 
 /* vat exercise 2 */
+function vat2(priceIncludingVat, vatpercentage) {
+  return priceIncludingVat / (vatpercentage / 100 + 1);
+}
+
+function basePrice(priceIncludingVat, vatpercentage) {
+  return [vat2(priceIncludingVat, vatpercentage), vatpercentage];
+}
+
+console.log(basePrice(2.18, 9));
