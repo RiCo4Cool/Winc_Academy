7bc739d421808b3e900c708b0816da50b1bd875a
Richard Cornelissen <ricorn@gmail.com>
2021-07-06 13:59:32 +0200

    final

--- function_basics_exercise/script.js
+++ function_basics_exercise/script.js
@@ -4,6 +4,14 @@ const makePizza = function () {
   console.log("Use the oven");
 };
 
-makePizza();
+const makeSushi = function () {
+  console.log("Get the rice");
+  console.log("Get the fish");
+  console.log("Roll up in a bamboo mat");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
 makePizza();
 makePizza();
