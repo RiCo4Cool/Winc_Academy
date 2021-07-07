4a279accbb5d1a799753eee8283f9edf9b7056c5
Richard Cornelissen <ricorn@gmail.com>
2021-07-07 13:21:06 +0200

    evenodd

--- while_loops_&_for_loops_exercise/even-odd-reporter.js
+++ while_loops_&_for_loops_exercise/even-odd-reporter.js
@@ -1,7 +1,12 @@
 console.log(`Is i even or odd?`);
 
 for (let i = 1; i <= 20; i++) {
-  console.log(`${i} is`);
+  if (i % 2 == 0) {
+    console.log(`${i} is even`);
+  } else {
+    console.log(`${i} is oneven`);
+  }
+
   // TODO: determine if i is odd or even
   // TODO: use console.log
 }
