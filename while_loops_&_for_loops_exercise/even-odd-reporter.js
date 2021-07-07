3df3fe33c758b3c07ba8ea9336891eba6c07eef9
Richard Cornelissen <ricorn@gmail.com>
2021-07-07 13:18:46 +0200

    consolelog

---
+++ while_loops_&_for_loops_exercise/even-odd-reporter.js
@@ -0,0 +1,14 @@
+console.log(`Is i even or odd?`);
+
+for (let i = 1; i <= 20; i++) {
+  console.log(`${i} is`);
+  // TODO: determine if i is odd or even
+  // TODO: use console.log
+}
+
+// Expected output:
+// 1 is oneven
+// 2 is even
+// 3 is oneven
+// 4 is even
+// ...
