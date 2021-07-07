78828814337245ede711312a1b89c5bb4e1203d2
Richard Cornelissen <ricorn@gmail.com>
2021-07-07 13:41:06 +0200

    nested

---
+++ while_loops_&_for_loops_exercise/multiplication-tables.js
@@ -0,0 +1,9 @@
+// for (i = 1; i <= 20; i++) {
+//   console.log(`${i} x 9 = ${i * 9}`);
+// }
+
+for (i = 1; i <= 10; i++) {
+  for (j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`);
+  }
+}
