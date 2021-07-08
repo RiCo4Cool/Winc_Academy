f0a64188dde3fd5d555051224c79d226e7b18f32
Richard Cornelissen <ricorn@gmail.com>
2021-07-08 14:37:26 +0200

    blue

--- gebruik_eventhandlers_defer/script.js
+++ gebruik_eventhandlers_defer/script.js
@@ -1,6 +1,3 @@
 document.querySelector(".btn-alert").addEventListener("click", function () {
   alert("button clicked");
 });
-var btn = document.createElement("button");
-btn.innerHTML = "Change Background";
-document.body.appendChild(btn);
