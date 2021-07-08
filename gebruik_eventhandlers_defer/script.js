e2fe53c833462432f19cd65857b8ac05b63d5a48
Richard Cornelissen <ricorn@gmail.com>
2021-07-08 14:47:46 +0200

    red

--- gebruik_eventhandlers_defer/script.js
+++ gebruik_eventhandlers_defer/script.js
@@ -1,3 +1,6 @@
-document.querySelector(".btn-alert").addEventListener("click", function () {
+document.querySelector("#mybutton").addEventListener("click", function () {
   alert("button clicked");
+});
+document.querySelector("#backgbutton").addEventListener("click", function () {
+  document.body.classList.add("red-background");
 });
