4cba829521903286852a9e63441b6d0c93862e9c
Richard Cornelissen <ricorn@gmail.com>
2021-07-08 14:49:27 +0200

    toggle

--- gebruik_eventhandlers_defer/script.js
+++ gebruik_eventhandlers_defer/script.js
@@ -2,5 +2,5 @@ document.querySelector("#mybutton").addEventListener("click", function () {
   alert("button clicked");
 });
 document.querySelector("#backgbutton").addEventListener("click", function () {
-  document.body.classList.add("red-background");
+  document.body.classList.toggle("red-background");
 });
