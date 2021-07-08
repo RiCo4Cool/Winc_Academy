7bdd023ae2597e6a5f29ea8d729a43bd978c32d4
Richard Cornelissen <ricorn@gmail.com>
2021-07-08 16:34:35 +0200

    dropdownremove

--- eindopdracht_kleurentoggle/script.js
+++ eindopdracht_kleurentoggle/script.js
@@ -2,3 +2,13 @@ document.getElementById("dropdown").style.display = "none";
 document.querySelector("#hamburger").addEventListener("click", function () {
   document.getElementById("dropdown").style.display = "block";
 });
+
+document.querySelector("#red").addEventListener("click", function () {
+  document.body.style.backgroundColor = "red";
+  document.querySelector("#dropdown").remove();
+});
+
+document.querySelector("#green").addEventListener("click", function () {
+  document.body.style.backgroundColor = "green";
+  document.querySelector("#dropdown").remove();
+});
