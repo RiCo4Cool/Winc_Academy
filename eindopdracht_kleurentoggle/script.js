0da0f0f86dd185e6656de3aba1ca2f66ccf70903
Richard Cornelissen <ricorn@gmail.com>
2021-07-08 16:24:14 +0200

    block

--- eindopdracht_kleurentoggle/script.js
+++ eindopdracht_kleurentoggle/script.js
@@ -1,3 +1,4 @@
-document.querySelector(".hamburger").addEventListener("click", function () {
-  alert("clicked!");
+document.getElementById("dropdown").style.display = "none";
+document.querySelector("#hamburger").addEventListener("click", function () {
+  document.getElementById("dropdown").style.display = "block";
 });
