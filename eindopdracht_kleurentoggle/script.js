f1f54a40b4074fe9fddae5ba170c572c1a23d50c
Richard Cornelissen <ricorn@gmail.com>
2021-07-12 21:03:57 +0200

    menudisplaynone

--- eindopdracht_kleurentoggle/script.js
+++ eindopdracht_kleurentoggle/script.js
@@ -1,33 +1,33 @@
-var click = false;
-
 document.querySelector("#hamburger").addEventListener("click", function () {
-  if (click == false) {
-    document
-      .getElementById("red")
-      .appendChild(
-        document.createElement("li").appendChild(document.createTextNode("Red"))
-      );
-    document
-      .getElementById("green")
-      .appendChild(
-        document
-          .createElement("li")
-          .appendChild(document.createTextNode("Green"))
-      );
-    click = true;
-  }
+  document.getElementById("menu").style.display = "block";
+});
+
+document.querySelector("#home").addEventListener("click", function () {
+  document.body.style.backgroundColor = "transparent";
+  document.getElementById("color").textContent = "";
+  document.getElementById("menu").style.display = "none";
 });
 
 document.querySelector("#red").addEventListener("click", function () {
   document.body.style.backgroundColor = "red";
-  document.querySelector("#menu").remove();
-  document.getElementById("color").appendChild(document.createTextNode("Red"));
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("color").textContent = "Rood";
 });
 
 document.querySelector("#green").addEventListener("click", function () {
   document.body.style.backgroundColor = "green";
-  document.querySelector("#menu").remove();
-  document
-    .getElementById("color")
-    .appendChild(document.createTextNode("Green"));
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("color").textContent = "Groen";
+});
+
+document.querySelector("#yellow").addEventListener("click", function () {
+  document.body.style.backgroundColor = "yellow";
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("color").textContent = "Geel";
+});
+
+document.querySelector("#blue").addEventListener("click", function () {
+  document.body.style.backgroundColor = "lightblue";
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("color").textContent = "Blauw";
 });
